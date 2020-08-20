23a786c2b50952b5cb2e3baeb0e9c9eea7092593
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-20 13:38:45 +0200

    fix: remove localhost from app.listen (needed in local computer)

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const controller = require('./controllers/postController');
 const app = express();
+const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json()); 
 
 controller.postController(app);
 
-app.listen(3000, 'localhost', () => {
-    console.log("The app is listening on port 3000...");
+app.listen(PORT, () => {
+    console.log(`The app is listening on port ${PORT}...`);
 });
