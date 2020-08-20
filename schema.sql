3df93879dc62d8a7c331e81b9fa7fcbb845d0319
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-20 13:35:26 +0200

    fix: make table names plural

--- schema.sql
+++ schema.sql
@@ -11,40 +11,39 @@ USE `reddit`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Table structure for table `posts`
 --
-DROP TABLE IF EXISTS `post`;
-CREATE TABLE IF NOT EXISTS `post` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(100) NOT NULL DEFAULT '',
-    `url` VARCHAR(150) NOT NULL DEFAULT '',
-    `timestamp` INT NOT NULL,
-    `user_id` INT NOT NULL DEFAULT 0,
-    `score` INT NOT NULL DEFAULT 0,
-
-    PRIMARY KEY (`id`)
-);
-
---
--- Table structure for table `user`
---
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user2` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(45) NOT NULL DEFAULT '',
-
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX (`username`)
-);
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `url` varchar(150) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `timestamp` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `score` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Table structure for table `vote`
+-- Table structure for table `users`
 --
-DROP TABLE IF EXISTS `vote`;
-CREATE TABLE IF NOT EXISTS `vote` (
-    `post_id` INT NOT NULL,
-    `user_id` INT NOT NULL,
-    `vote` TINYINT NOT NULL DEFAULT 0,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-    PRIMARY KEY (`post_id`, `user_id`)
-);
+
+--
+-- Table structure for table `vote`s
+--
+DROP TABLE IF EXISTS `votes`;
+CREATE TABLE IF NOT EXISTS `votes` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `vote` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
